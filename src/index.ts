38dc3cf34eebe443a6b8580de3aebe8dd4145cc2
Ralph Christian D. Eclipse <ralphchristianeclipse@gmail.com>
2019-09-05 09:03:34 +0800

    :sparkles: handle reloading captcha

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import Tesseract from 'tesseract.js';
 import { resolve } from 'dns';
 
-const { TesseractWorker } = Tesseract;
+const { TesseractWorker }: any = Tesseract;
 const worker = new TesseractWorker();
 
 const getProgress = (img, progress) =>
@@ -20,39 +20,67 @@ const getProgress = (img, progress) =>
 
 const app = express();
 const sendMessages = async ({ phone, message }) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false
+  });
   const page = await browser.newPage();
   await page.goto('http://www.afreesms.com/intl/philippines', {
     waitUntil: 'networkidle2'
   });
   const navigationPromise = page.waitForNavigation();
-  const [ids, idsT, url]: any = await page.evaluate(() => {
+
+  const [ids, idsT]: any = await page.evaluate(async () => {
     const inputs = Array.from(document.querySelectorAll('input'));
     const textareas = Array.from(document.querySelectorAll('textarea'));
     const ids = inputs.map(({ id }) => id);
     const idsT = textareas.map(({ id }) => id);
-    const captchaImageEl = document.querySelector('#captcha');
-    const getDataUrl = img => {
-      var canvas = document.createElement('canvas');
-      var ctx = canvas.getContext('2d');
 
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
-
-      // If the image is not png, the format
-      // must be specified here
-      return canvas.toDataURL();
-    };
-    const url = getDataUrl(captchaImageEl);
-    return [ids, idsT, url];
+    return [ids, idsT];
   });
-  const text: string = (await getProgress(url, console.log)) as string;
-  const captchaText = text.slice(0, 6);
-  if (Number.isNaN(+captchaText))
-    throw new Error(
-      `Failed to send message to phone number: ${phone} due to captcha error ${captchaText}`
-    );
+
+  const evaluateCaptchaText = () =>
+    page.evaluate(() => {
+      const getDataUrlCaptcha = async () => {
+        const getImageEl = () =>
+          new Promise((resolve, reject) => {
+            const captchaImageEl: any = document.querySelector('#captcha');
+
+            const reloadEl: any = document.querySelector(
+              "img[alt='Reload the image']"
+            );
+            reloadEl.click();
+            captchaImageEl.onload = () => {
+              resolve(captchaImageEl);
+            };
+            captchaImageEl.onerror = reject;
+          });
+        const getDataUrl = img => {
+          var canvas = document.createElement('canvas');
+          var ctx = canvas.getContext('2d');
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+          // If the image is not png, the format
+          // must be specified here
+          return canvas.toDataURL();
+        };
+        const el = await getImageEl();
+        const url = getDataUrl(el);
+        return url;
+      };
+      return getDataUrlCaptcha();
+    });
+  const getCaptchaText = async () => {
+    const url = await evaluateCaptchaText();
+    const text: string = (await getProgress(url, console.log)) as string;
+    const captchaText = text.slice(0, 6);
+    console.log({ captchaText, text });
+    if (!text || !captchaText || Number.isNaN(+captchaText)) {
+      await evaluateCaptchaText();
+    }
+    return captchaText;
+  };
+  const captchaText = await getCaptchaText();
   const [, , mobileInputId, , captchaInputId] = ids;
   const [messageInputId] = idsT;
   const mobileNumberSelector = `[id="${mobileInputId}"]`;
@@ -68,15 +96,18 @@ const sendMessages = async ({ phone, message }) => {
   await page.click('table #submit');
   await navigationPromise;
   await browser.close();
-  return 'test';
+  return captchaText;
 };
 
 app.get('/send', async (req, res) => {
   const { phone, message } = req.query;
-
-  console.log({ phone, message });
-  const result = await sendMessages({ phone, message });
-  res.send(result);
+  try {
+    console.log({ phone, message });
+    const result = await sendMessages({ phone, message });
+    res.send(result);
+  } catch (e) {
+    res.send({ error: e.message });
+  }
 });
 const { PORT = 5000 } = process.env;
 
